656e63c950a53d1213eec1affe3301a77f5d05b6
lillicoder <scott@lillicoder.com>
2017-01-17 00:39:57 -0500

    Updated DemoActivity to extend AppCompatActivity for toolbar support

--- app/src/main/java/com/lillicoder/demo/DemoActivity.java
+++ app/src/main/java/com/lillicoder/demo/DemoActivity.java
@@ -16,8 +16,8 @@
 
 package com.lillicoder.demo.viewgroupmerge;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class DemoActivity extends ActionBarActivity {
+public class DemoActivity extends AppCompatActivity {
 
 }
